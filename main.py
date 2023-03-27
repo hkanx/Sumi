92adfeb15d8731fe161c7d1fb9a052f1fed76c11
hkanx <35790348+hkanx@users.noreply.github.com>
2023-03-27 11:52:31 -0700

    Update main.py
    uses chatGPT to answer a single question if the keyword phrase is in the HTML of the given URL.

--- main.py
+++ main.py
@@ -1,34 +1,38 @@
+import openai
 import requests
 from bs4 import BeautifulSoup
-# from chatgpt_wrapper import ChatGPT
 
-# bot = ChatGPT()
-# # return the full result. This is much slower
-# response = bot.ask("tell me a story about cats and dogs")
-# print(response)
+# Set your OpenAI API key
+openai.api_key = "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"
 
-# Set the URL of the webpage to scrape
-url = 'https://clinicaltrials.gov/ct2/show/results/NCT00934544?recrs=e&rslt=With&type=Intr&cond=Myelofibrosis&phase=2&draw=2&rank=1'
+# Define a function to scrape the HTML content of a webpage and search for a keyword phrase
+def search_webpage_for_phrase(url, phrase):
+    # Use requests to get the HTML content of the webpage
+    response = requests.get(url)
+    html_content = response.content
 
-# Set the keyword phrase to search for
-keyword = 'spleen volume'
+    # Use BeautifulSoup to parse the HTML content
+    soup = BeautifulSoup(html_content, 'html.parser')
 
-# Make a request to the webpage and get the HTML content
-response = requests.get(url)
-html_content = response.text
+    # Search for the keyword phrase in the text content of the page
+    text_content = soup.get_text()
+    if phrase in text_content:
+        # If the phrase is found, use ChatGPT to generate a response
+        prompt = f"What was the total number of patients analyzed for the different outcomes?"
+        response = openai.Completion.create(
+            engine="text-davinci-002",
+            prompt=prompt,
+            max_tokens=1024,
+            n=1,
+            stop=None,
+            temperature=0.7
+        )
+        return response.choices[0].text.strip()
+    else:
+        return f"The phrase '{phrase}' was not found on {url}"
 
-# Parse the HTML content using BeautifulSoup
-soup = BeautifulSoup(html_content, 'html.parser')
-
-# Find all the text on the webpage and check if the keyword phrase is present
-found_keyword = False
-for text in soup.stripped_strings:
-    if keyword in text:
-        found_keyword = True
-        break
-
-# Print the result
-if found_keyword:
-    print(f'The keyword "{keyword}" was found on the webpage.')
-else:
-    print(f'The keyword "{keyword}" was not found on the webpage.')
+# Call the search_webpage_for_phrase function with the URL and keyword phrase you want to search for
+url = "https://clinicaltrials.gov/ct2/show/results/NCT00934544?recrs=e&rslt=With&type=Intr&cond=Myelofibrosis&phase=2&draw=2&rank=1"
+phrase = "spleen volume"
+result = search_webpage_for_phrase(url, phrase)
+print(result)
