b5e64a643babc266f00498a1d31fd13de76afec0
hkanx <35790348+hkanx@users.noreply.github.com>
2023-03-27 14:38:12 -0700

    Update main.py
    incomplete: cannot find spleen research table with the word spleen reduction in it. Now prints to a CSV file!

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
+import csv
 import openai
 import requests
 from bs4 import BeautifulSoup
 
 # Set your OpenAI API key
-openai.api_key = "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"
+openai.api_key = "ENTER-API"
 
-# Define a function to scrape the HTML content of a webpage and search for a keyword phrase
-def search_webpage_for_phrase(url, phrase):
+# Define a function to scrape the HTML content of a webpage and search for the total number of patients analyzed at week 24
+def search_for_week24_patients(url):
     # Use requests to get the HTML content of the webpage
     response = requests.get(url)
     html_content = response.content
@@ -14,25 +15,45 @@ def search_webpage_for_phrase(url, phrase):
     # Use BeautifulSoup to parse the HTML content
     soup = BeautifulSoup(html_content, 'html.parser')
 
-    # Search for the keyword phrase in the text content of the page
-    text_content = soup.get_text()
-    if phrase in text_content:
-        # If the phrase is found, use ChatGPT to generate a response
-        prompt = f"What was the total number of patients analyzed for the different outcomes?"
-        response = openai.Completion.create(
-            engine="text-davinci-002",
-            prompt=prompt,
-            max_tokens=1024,
-            n=1,
-            stop=None,
-            temperature=0.7
-        )
-        return response.choices[0].text.strip()
-    else:
-        return f"The phrase '{phrase}' was not found on {url}"
+    # Search for the "Spleen response assessment" table in the HTML content
+    table = soup.find("table", {"id": "results_spleen"})
+    if table:
+        # If the table is found, search for the "Week 24" row in the table
+        rows = table.find_all("tr")
+        for row in rows:
+            columns = row.find_all("th")
+            if columns and "Time Frame" in columns[0].get_text().strip() and "Week 24" in row.get_text().strip():
+                week24_row = row
+                break
 
-# Call the search_webpage_for_phrase function with the URL and keyword phrase you want to search for
+        # If the "Week 24" row is found, search for the "Number of patients analyzed" cell in the row
+        cells = week24_row.find_all("td")
+        for cell in cells:
+            if "Number of patients analyzed" in cell.get_text().strip():
+                num_patients_analyzed = cell.find_next_sibling().get_text().strip()
+                # If the "Number of patients analyzed" cell is found, use ChatGPT to generate a response
+                prompt = f"Can you tell me more about spleen reduction on {url}? How many patients were analyzed at week 24?"
+                response = openai.Completion.create(
+                    engine="text-davinci-002",
+                    prompt=prompt,
+                    max_tokens=1024,
+                    n=1,
+                    stop=None,
+                    temperature=0.7
+                )
+                return response.choices[0].text.strip()
+        return f"No 'Number of patients analyzed' cell was found in the 'Week 24' row on {url}"
+    else:
+        return f"No spleen response assessment was found on {url}"
+
+# Call the search_for_week24_patients function with the URL you want to search
 url = "https://clinicaltrials.gov/ct2/show/results/NCT00934544?recrs=e&rslt=With&type=Intr&cond=Myelofibrosis&phase=2&draw=2&rank=1"
-phrase = "spleen volume"
-result = search_webpage_for_phrase(url, phrase)
+result = search_for_week24_patients(url)
+
+# Save the result to a CSV file
+with open('results.csv', mode='w') as results_file:
+    writer = csv.writer(results_file)
+    writer.writerow(['URL', 'Result'])
+    writer.writerow([url, result])
+
 print(result)
