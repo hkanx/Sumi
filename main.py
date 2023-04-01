a566a372b3485762318da4914889951a4ffd7e5f
hkanx <35790348+hkanx@users.noreply.github.com>
2023-04-01 02:27:28 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,37 +3,56 @@ import requests
 from bs4 import BeautifulSoup
 from chatgpt_wrapper import ChatGPT
 import csv
+import re
 
-# # Integrate chatGPT with features
-# bot = ChatGPT()
+# Gameplan: 
+#   -   With a given URL, scrape all the text on the webpage.
+# 	⁃	chunk up the text on the website 
+# 	⁃	Feed chunked up texts to chatpgpt to    
+#           1) check whether its what you’re looking for 
+#                - Ask chatGPT if the words found talks about at Least 35% Reduction in Spleen Volume From Baseline at Week 24
+#           2) and get the results you want
+#               - If so, then ask chatGPT the different treatment options and the number of patients per treatment
+# 	⁃	If not, go to the next keyword occurrence 
 
-# response = bot.ask("in what section does the word?")
-# print(response)
 
-# Make a GET request to the webpage you want to search
-url = "https://clinicaltrials.gov/ct2/show/results/NCT00934544?rslt=With&type=Intr&cond=Myelofibrosis&phase=2&draw=2&rank=1 "
+# Make an HTTP request to the webpage
+url = 'https://en.wikipedia.org/wiki/Python_(programming_language)'
 response = requests.get(url)
 
 # Parse the HTML content using BeautifulSoup
-soup = BeautifulSoup(response.content, "html.parser")
+soup = BeautifulSoup(response.content, 'html.parser')
 
-# Find all occurrences of a keyword in the webpage
-keyword = "safety"
-elements = soup.find_all(string=lambda text: text and keyword in text)
+#print(response.content)
 
-if elements:
-    # Output the locations of all keyword occurrences to the console
-    for element in elements:
-        location = element.parent
-        print(location)
+# Find all the text elements on the webpage
+text_elements = soup.find_all(string=True)
 
-    # Write the locations of all keyword occurrences to a file
-    with open("keyword_locations.txt", "w") as f:
-        f.write(f"Found {len(elements)} occurrences of keyword '{keyword}' on the webpage:\n")
-        for i, element in enumerate(elements):
-            
-            location = element.parent
-            f.write(f"Keyword occurrence {i+1} found in element: {location}\n")
-            f.write("="*80 + "\n")
-else:
-    print(f"Could not find keyword '{keyword}' on the webpage")
+# Combine the text elements into a single string
+text = ' '.join(text_elements)
+
+# Define the regex pattern to match section titles
+pattern = r'^\s*(Chapter|Section|Part|Chapter \d+|Section \d+|Part \d+|[A-Z][A-Za-z\s&]*)\s*$'
+
+# Split the text into sections based on the section titles
+sections = {}
+current_title = None
+for line in text.splitlines():
+    match = re.match(pattern, line)
+    if match:
+        current_title = match.group(0).strip()
+        sections[current_title] = []
+    elif current_title is not None:
+        sections[current_title].append(line.strip())
+        
+print(sections)
+
+# Convert the sections dictionary to a string with section titles and content
+result = ''
+for title, content in sections.items():
+    result += title + '\n'
+    result += '\n'.join(content) + '\n\n'
+
+print(result)
+
+
